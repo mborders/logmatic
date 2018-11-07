0d1054ff33cdc87630301934b9008a9d193fee0d
Matthew Borders <mborders@outlook.com>
2018-11-06 20:41:48 -0600

    Added unit tests for logger

---
+++ logger_test.go
@@ -0,0 +1,100 @@
+package logmatic
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func mockPrint(buf *bytes.Buffer) {
+	printf = func(format string, a ...interface{}) (int, error) {
+		buf.WriteString(fmt.Sprintf(format, a...))
+		return 0, nil
+	}
+}
+
+func TestLogger_Trace_Print(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.SetLevel(TRACE)
+	l.Trace("Trace")
+	assert.NotEmpty(t, buf)
+}
+
+func TestLogger_Trace_NoPrint(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.Trace("Trace")
+	assert.Empty(t, buf)
+}
+
+func TestLogger_Debug_Print(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.SetLevel(DEBUG)
+	l.Debug("Debug")
+	assert.NotEmpty(t, buf)
+}
+
+func TestLogger_Debug_NoPrint(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.Debug("Debug")
+	assert.Empty(t, buf)
+}
+
+func TestLogger_Info_Print(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.Info("Info")
+	assert.NotEmpty(t, buf)
+}
+
+func TestLogger_Info_NoPrint(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.SetLevel(WARN)
+	l.Info("Info")
+	assert.Empty(t, buf)
+}
+
+func TestLogger_Warn_Print(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.Warn("Warn")
+	assert.NotEmpty(t, buf)
+}
+
+func TestLogger_Warn_NoPrint(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.SetLevel(ERROR)
+	l.Warn("Warn")
+	assert.Empty(t, buf)
+}
+
+func TestLogger_Error_Print(t *testing.T) {
+	var buf bytes.Buffer
+	mockPrint(&buf)
+
+	l := NewLogger()
+	l.Error("Error")
+	assert.NotEmpty(t, buf)
+}
