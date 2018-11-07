df7c0525ea773232da65422a774bbb3aa1c20dae
Matthew Borders <mborders@outlook.com>
2018-11-06 20:41:18 -0600

    Added log levels

---
+++ level.go
@@ -0,0 +1,14 @@
+package logmatic
+
+// LogLevel controls what log statements are
+// activated for a given logger
+type LogLevel uint8
+
+// Log levels
+const (
+	TRACE = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+)
